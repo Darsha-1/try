65ac56fb4734affa848256140acee6832ce02fd8
Darsha-1 <97642863+Darsha-1@users.noreply.github.com>
2022-01-14 23:46:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+
+<head><title> Add Info .... </title></head>
+
+<body>
+
+<form action="add_info.php" method="post">
+<table>
+<tr>
+<td> Item1  : </td>
+<td><input type="text" name="item1" /></td>
+
+</tr>
+
+<tr>
+<td> Quantity1  : </td>
+<td><input type="number" name="quantity1" /></td>
+</tr>
+
+</table>
+<input type="submit" value = "submit info"/>
+</form>
+</body>
+
+</html>
